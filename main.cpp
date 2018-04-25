db5f1118474fc1a0d5894cef598bae1a886bc5dd
Jago <jago.coric@hotmail.com>
2018-04-25 22:00:00 +0200

    The updated files for ptrception

--- main.cpp
+++ main.cpp
@@ -6,15 +6,18 @@ int main(){
     float y = 1;
     //float* px = &x; 
     //float* py = &y;
-    neuron pi(2);
+    //neuron pi(2);
 
-    cout << pi.resultFunc({&x,&y}) << endl;
-    cout << *pi.resultFunc({&x,&y}) << endl;
+    //cout << pi.resultFunc({&x,&y}) << endl;
+    //cout << *pi.resultFunc({&x,&y}) << endl;
 
     float xx = 0;
     float yy = 1;
     float zz = 1;
-    neuron po({0.4, 0.6, 1}, 0.7);
+    vector<float> ww = {0.4, 0.6, 1};
+    vector<float*> www={&ww[0],&ww[1],&ww[2]};
+    float bb = 0.7;
+    neuron po(&www, &bb);
     cout <<*po.resultFunc({&xx,&yy,&zz}) << endl;
 
     return 0;
